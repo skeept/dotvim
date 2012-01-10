6a5536981cf446ad02f14cd57614f337bcfb9734
Helder Jorge Rodrigues Inacio <skeept@gmail.com>
2012-01-10 14:28:31 -0500

    plugin update: ultisnips

--- bundle/ultisnips_rep/test.py
+++ bundle/ultisnips_rep/test.py
@@ -1928,6 +1928,23 @@ class Snippet_With_Umlauts(_VimTest):
     keys = 'teüst' + EX
     wanted = "Expand meü!"
 
+class Snippet_With_Umlauts_TypeOn(_VimTest):
+    snippets = ('ül', 'üüüüüßßßß')
+    keys = 'te ül' + EX + "more text"
+    wanted = "te üüüüüßßßßmore text"
+class Snippet_With_Umlauts_OverwriteFirst(_VimTest):
+    snippets = ('ül', 'üü ${1:world} üü ${2:hello}ßß\nüüüü')
+    keys = 'te ül' + EX + "more text" + JF + JF + "end"
+    wanted = "te üü more text üü helloßß\nüüüüend"
+class Snippet_With_Umlauts_OverwriteSecond(_VimTest):
+    snippets = ('ül', 'üü ${1:world} üü ${2:hello}ßß\nüüüü')
+    keys = 'te ül' + EX + JF + "more text" + JF + "end"
+    wanted = "te üü world üü more textßß\nüüüüend"
+class Snippet_With_Umlauts_OverwriteNone(_VimTest):
+    snippets = ('ül', 'üü ${1:world} üü ${2:hello}ßß\nüüüü')
+    keys = 'te ül' + EX + JF + JF + "end"
+    wanted = "te üü world üü helloßß\nüüüüend"
+
 class Snippet_With_DoubleQuote_List(_VimTest):
     snippets = _snip_quote('"')
     keys = "te" + LS + "2\n"
@@ -2337,6 +2354,27 @@ hi4"""
 
     def _options_on(self):
         self.send(":set langmap=dj,rk,nl,ln,jd,kr,DJ,RK,NL,LN,JD,KR\n")
+    def _options_off(self):
+        self.send(":set langmap=\n")
+
+#######################
+# Test for bug 871357 #
+#######################
+class TestLangmapWithUtf8_ExceptCorrectResult(_VimTest):
+    snippets = ("testme",
+"""my snipped ${1:some_default}
+and a mirror: $1
+$2...$3
+$0""")
+    keys = "testme" + EX + "hi1" + JF + "hi2" + JF + "hi3" + JF + "hi4"
+    wanted ="""my snipped hi1
+and a mirror: hi1
+hi2...hi3
+hi4"""
+
+    def _options_on(self):
+        self.send(":set langmap=йq,цw,уe,кr,еt,нy,гu,шi,щo,зp,х[,ъ],фa,ыs,вd,аf,пg,рh,оj,лk,дl,ж\\;,э',яz,чx,сc,мv,иb,тn,ьm,ю.,ё',ЙQ,ЦW,УE,КR,ЕT,НY,ГU,ШI,ЩO,ЗP,Х\{,Ъ\},ФA,ЫS,ВD,АF,ПG,РH,ОJ,ЛK,ДL,Ж\:,Э\",ЯZ,ЧX,СC,МV,ИB,ТN,ЬM,Б\<,Ю\>\n")
+
     def _options_off(self):
         self.send(":set langmap=\n")
 
