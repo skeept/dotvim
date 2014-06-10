66f9ed34654aada61bfb4decc4f4dad6a7a891db
Helder Jorge Rodrigues Inacio <skeept@gmail.com>
2014-06-10 10:03:44 -0400

    get updated for ack pathogen

---
+++ notused_plugins/F/copy_to_sub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+cp ack/autoload/* ~/.vim/autoload/
+cp ack/plugin/* ~/.vim/plugin/
+cp ack/doc/* ~/.vim/doc/
+
+cp unimpaired/plugin/* ~/.vim/plugin/
+cp unimpaired/doc/* ~/.vim/doc/
+
+cp pathogen/autoload/* ~/.vim/autoload/
+
+cp Recover/autoload/* ~/.vim/autoload/
+cp Recover/plugin/* ~/.vim/plugin/
+cp Recover/doc/* ~/.vim/doc/
