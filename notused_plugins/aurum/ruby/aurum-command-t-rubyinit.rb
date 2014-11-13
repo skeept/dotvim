5d3486d5292aa8b87d6c022b6b93dbb1ee382307
Helder Jorge Rodrigues Inacio <skeept@gmail.com>
2014-11-12 20:07:40 -0500

    airline syntastic neocomplete unite aurum

--- notused_plugins/aurum/ruby/aurum-command-t-rubyinit.rb
+++ notused_plugins/aurum/ruby/aurum-command-t-rubyinit.rb
@@ -1,3 +1,10 @@
-require 'command-t/aurum_controller'
+require 'command-t/finder/aurum_finder'
 require 'command-t/vim'
-$aurum_command_t = CommandT::AurumController.new
+def $command_t.aurum_finder
+    CommandT::AurumFinder.new
+end
+def $command_t.show_aurum_finder
+    @path          = CommandT::VIM::pwd
+    @active_finder = aurum_finder
+    show
+end
