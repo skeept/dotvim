009d2c5260bb65fd537ffd3a6ca05d14aea593e7
Helder Jorge Rodrigues Inacio <skeept@gmail.com>
2015-07-27 21:20:47 -0400

    ultisnips

--- update_addons.sh
+++ update_addons.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+
 (cd bundle/ ; sh move2_git.sh)
 
 (cd notused_plugins/ ; sh ../bundle/move2_git.sh)
 
 (cd notused_plugins/F ; sh ../../bundle/move2_git.sh)
 
-#(cd notused_plugins/Infrequent/ ; sh ../../bundle/move2_git.sh)
+if test "a$1" = "a-a"; then
+  (cd notused_plugins/Infrequent/ ; sh ../../bundle/move2_git.sh)
+fi
 
