7e7b45a006471a2e1fd30bba903176f8f6c00426
U-Middle-Earth\Alatar <skeept@gmail.com>
2017-07-20 20:42:46 -0500

    ale fzf update_addons.sh

--- update_addons.sh
+++ update_addons.sh
@@ -7,11 +7,6 @@ PROG="$(readlink -f bundle/move2_git.sh)"
 
 (cd bundle/ ; sh "$PROG")
 
-(cd notused_plugins/ ; sh "$PROG")
+(cd pack/bundle/start/ ; sh "$PROG")
 
-(cd notused_plugins/F ; sh "$PROG")
-
-if test "a$1" = "a-a"; then
-  (cd notused_plugins/Infrequent/ ; sh "$PROG")
-fi
-
+(cd pack/bundle/opt/ ; sh "$PROG")
