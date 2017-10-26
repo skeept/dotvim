1716fa3f2ac44db9ee33cd010c340608f0bfaea2
U-Middle-Earth\Alatar <skeept@gmail.com>
2017-10-25 20:35:37 -0500

    ale airline fzf rpc vam yarp

--- pack/bundle/opt/neovim-rpc/pythonx/neovim_rpc_protocol.py
+++ pack/bundle/opt/neovim-rpc/pythonx/neovim_rpc_protocol.py
@@ -11,13 +11,20 @@ import neovim_rpc_methods
 import threading
 import socket
 import time
-from neovim.api.common import walk, decode_if_bytes
+from neovim.api.common import decode_if_bytes
 
 BUFFER_TYPE = type(vim.current.buffer)
 BUFFER_TYPE_ID = neovim_rpc_server_api_info.API_INFO['types']['Buffer']['id']
 WINDOW_TYPE = type(vim.current.window)
 WINDOW_TYPE_ID = neovim_rpc_server_api_info.API_INFO['types']['Window']['id']
 
+def walk(fn, obj):
+    if type(obj) in [list, tuple, vim.List]:
+        return list(walk(fn, o) for o in obj)
+    if type(obj) in [dict, vim.Dictionary]:
+        return dict((walk(fn, k), walk(fn, v)) for k, v in
+                    obj.items())
+    return fn(obj)
 
 def from_client(msg):
 
@@ -40,6 +47,8 @@ def to_client(msg):
             return msgpack.ExtType(BUFFER_TYPE_ID, msgpack.packb(obj.number))
         if type(obj) == WINDOW_TYPE:
             return msgpack.ExtType(WINDOW_TYPE_ID, msgpack.packb(obj.number))
+        if type(obj) == vim.Function:
+            return obj.name
         return obj
     return walk(handler, msg)
 
