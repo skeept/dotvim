65f63a8234319f492d75f0ccbfb9d28450f2e9b9
U-Middle-Earth\Alatar <skeept@gmail.com>
2019-12-26 16:57:10 -0600

    do not use bundle folder

--- update_addons.sh
+++ update_addons.sh
@@ -2,10 +2,8 @@
 
 unset GREP_OPTIONS
 
-PROG="$(readlink -f bundle/move2_git.sh)"
+PROG="$(readlink -f pack/bundle/opt/move2_git.sh)"
 
-
-(cd bundle/ ; sh "$PROG")
 
 (cd pack/bundle/start/ ; sh "$PROG")
 
