003e7f27e7aab5c36c3bd0cb4aab7961b1dbad6f
U-Middle-Earth\Alatar <skeept@gmail.com>
2020-12-24 12:37:45 -0600

    dente deoplete

--- pack/bundle/opt/deoplete/rplugin/python3/deoplete/filter/matcher_matchfuzzy.py
+++ pack/bundle/opt/deoplete/rplugin/python3/deoplete/filter/matcher_matchfuzzy.py
@@ -1,25 +1,26 @@
 # ============================================================================
-# FILE: converter_remove_paren.py
+# FILE: matcher_matchfuzzy.py
 # AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
 # License: MIT license
 # ============================================================================
-
-import re
 
 from deoplete.base.filter import Base
 from deoplete.util import Nvim, UserContext, Candidates
 
 
 class Filter(Base):
+
     def __init__(self, vim: Nvim) -> None:
         super().__init__(vim)
 
-        self.name = 'converter_remove_paren'
-        self.description = 'remove parentheses converter'
+        self.name = 'matcher_matchfuzzy'
+        self.description = 'matchfuzzy() matcher'
 
     def filter(self, context: UserContext) -> Candidates:
-        for candidate in [x for x in context['candidates']
-                          if '(' in x['word']]:
-            candidate['word'] = re.sub(r'\(.*\)(\$\d+)?', '',
-                                       candidate['word'])
-        return list(context['candidates'])
+        if not self.vim.call('exists', '*matchfuzzy'):
+            return []
+
+        return list(self.vim.call(
+                        'matchfuzzy', context['candidates'],
+                        context['complete_str'], {'key': 'word'}
+                ))
