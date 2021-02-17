7aada42aa513a1c23768f1706374963888e26c6a
U-Middle-Earth\Alatar <skeept@gmail.com>
2021-02-17 14:20:40 -0600

    denite

--- pack/bundle/opt/denite/rplugin/python3/denite/source/help.py
+++ pack/bundle/opt/denite/rplugin/python3/denite/source/help.py
@@ -4,6 +4,8 @@
 # License: MIT license
 # ============================================================================
 
+from os import sep
+from pathlib import Path
 from pynvim import Nvim
 
 from denite.base.source import Base
@@ -16,15 +18,21 @@ class Source(Base):
         super().__init__(vim)
         self.vim = vim
         self.name = 'help'
-        self.kind = 'command'
+        self.kind = 'file'
 
     def gather_candidates(self, context: UserContext) -> Candidates:
         candidates: Candidates = []
         extend = candidates.extend
         for f in globruntime(context['runtimepath'], 'doc/tags'):
             with open(f, 'r') as ins:
-                extend(list(map(lambda canidate: {
-                    'word': canidate.split("\t", 1)[0],
-                    'action__command': 'silent h ' + canidate.split("\t", 1)[0]
+                root = str(Path(f).parent)
+                extend(list(map(lambda candidate: {
+                    'word': candidate.split("\t", 1)[0],
+                    'action__path': (
+                        root + sep + candidate.split("\t")[1]
+                    ),
+                    'action__pattern': (
+                        r'\V' + candidate.split("\t")[2].rstrip('\n')[1:]
+                    ),
                 }, ins)))
         return candidates
