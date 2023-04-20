71d41b489a5b4f97b5757950824e1dee1ec5dd3c
Helder Inacio <Helder.Inacio@Sabre.com>
2023-04-20 11:27:35 -0500

    ipython start.py scripot

--- test/keep/ipython_start.py
+++ test/keep/ipython_start.py
@@ -1,46 +1,67 @@
-import os
+# import os
 import sys
-from pathlib import Path
 import threading
 
-np = None
-pd = None
-sk = None
-tf = None
+# np = None
+# pd = None
+# sk = None
+# tf = None
+# Path = None
 
+def _load_heavy():
+    """Import on a thread."""
+    global os
+    import os
 
-def _load_pandas():
+    global Path
+    from pathlib import Path
+
     try:
         global pd
-        import pandas as pd_
-
-        pd = pd_
+        import pandas as pd
     except ImportError:
-        sys.stderr.write("cannot imort pandas\n")
+        sys.stderr.write("cannot import pandas\n")
 
     try:
         global np
-        import numpy as np_
-
-        np = np_
+        import numpy as np
     except ImportError:
         sys.stderr.write("cannot import numpy\n")
 
     try:
         global sk
-        import sklearn as sk_
-
-        sk = sk_
+        import sklearn as sk
     except ImportError:
         sys.stderr.write("cannot import sklearn\n")
 
+    # try:
+        # global tf
+        # import tensorflow as tf_
+        # tf = tf_
+    # except ImportError:
+        # sys.stderr.write("cannot import tensorflow\n")
+
     try:
         global tf
-        import tensorflow as tf_
-
-        tf = tf_
+        import tensorflow as tf
     except ImportError:
         sys.stderr.write("cannot import tensorflow\n")
 
+    try:
+        global rich
+        import rich
+        import functools
+        global ii
+        ii = functools.partial(rich.inspect, methods=True)
+        global iih
+        iih = functools.partial(rich.inspect, methods=True, help=True)
+        global iia
+        iia = functools.partial(rich.inspect, all=True)
+    except ImportError:
+        sys.stderr.write("cannot import rich\n")
 
-threading.Thread(target=_load_pandas).start()
+
+
+
+threading.Thread(target=_load_heavy).start()
+del _load_heavy
