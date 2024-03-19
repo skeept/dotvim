c887f8ef377830afd5fbb8e025c5386bf32aa3e5
Helder Inacio <Helder.Inacio@Sabre.com>
2024-03-19 09:22:32 -0500

    use an enum instead of explicit values

--- keep/nvim/copy_files.py
+++ keep/nvim/copy_files.py
@@ -1,6 +1,7 @@
 """Copy files to/from config folder."""
 
 import argparse
+import enum
 import os
 import filecmp
 import shutil
@@ -16,24 +17,38 @@ def get_all_elems(name: Path):
             yield from get_all_elems(elem)
 
 
-def copy_files(origin: Path, destination: Path, dry_run: bool = True) -> None:
+class CmpStatus(enum.Enum):
+    """Status of file comparision."""
+
+    same = enum.auto()
+    missing = enum.auto()
+    diff = enum.auto()
+
+
+def copy_files(
+    origin: Path, destination: Path, dry_run: bool = True, show_diff: bool = True
+) -> None:
     """Assume folders and copy nested files."""
+    show_diff = not dry_run or show_diff
     for elem in get_all_elems(origin):
-        missing_or_different = "."
+        missing_or_different = CmpStatus.same
         backup = destination / elem.relative_to(origin)
         if not backup.exists():
-            missing_or_different = "X"
+            missing_or_different = CmpStatus.missing
         elif not elem.is_dir() and not filecmp.cmp(elem, backup, shallow=False):
-            missing_or_different = "D"
-        print(f"{missing_or_different} {str(elem):<50} => {backup}")
-        if missing_or_different == "D" and shutil.which("delta"):
+            missing_or_different = CmpStatus.diff
+        print(f"{missing_or_different.name:<7} {str(elem):<50} => {backup}")
+        if (
+            show_diff
+            and missing_or_different == CmpStatus.diff
+            and shutil.which("delta")
+        ):
             subprocess.run(["delta", elem, backup])
-    if not dry_run:
-        for elem in get_all_elems(origin):
-            backup = destination / elem.relative_to(origin)
+
+        if not dry_run:
             if elem.is_dir():
                 backup.mkdir(exist_ok=True, parents=True)
-            else:
+            elif missing_or_different != CmpStatus.same:
                 shutil.copy(elem, backup)
 
 
@@ -42,6 +57,9 @@ def main() -> None:
     parser = argparse.ArgumentParser("copy files from folder.")
     parser.add_argument("-c", "--copy", help="copy from", action="store_true")
     parser.add_argument("-f", "--force", action="store_true", help="copy the files")
+    parser.add_argument(
+        "-d", "--hide_diff", action="store_true", help="hide diff output"
+    )
     args = parser.parse_args()
 
     nvim_conf_dir = (
@@ -56,7 +74,7 @@ def main() -> None:
 
     origin, dest = (nvim_conf, saved) if args.copy else (saved, nvim_conf)
     dry_run = not args.force
-    copy_files(origin, dest, dry_run=dry_run)
+    copy_files(origin, dest, dry_run=dry_run, show_diff=not args.hide_diff)
 
 
 if __name__ == "__main__":
