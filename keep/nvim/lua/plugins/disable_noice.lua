e8a566f39fd981967b1b595de596471e24ab181b
Helder Inacio <Helder.Inacio@sabre.com>
2024-03-30 09:53:21 -0500

    let's try scratch

--- keep/nvim/lua/plugins/disable_noice.lua
+++ keep/nvim/lua/plugins/disable_noice.lua
@@ -72,4 +72,17 @@ return {
     "cappyzawa/trim.nvim",
     opts = {},
   },
+  {
+  "https://git.sr.ht/~swaits/scratch.nvim",
+  lazy = true,
+  keys = {
+    { "<leader>bs", "<cmd>Scratch<cr>", desc = "Scratch Buffer", mode = "n" },
+    { "<leader>bS", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
+  },
+  cmd = {
+    "Scratch",
+    "ScratchSplit",
+  },
+  opts = {},
+}
 }
