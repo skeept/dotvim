6822ac93efa9eaaf0e635fd56be7ec8b89dfc802
Helder Inacio <Helder.Inacio@sabre.com>
2024-04-02 17:06:52 -0500

    create lua config for capture out scratch

--- keep/nvim/lua/config/keymaps.lua
+++ keep/nvim/lua/config/keymaps.lua
@@ -22,3 +22,33 @@ end
 vim.api.nvim_create_user_command("DeltTrailingSpace", DeltTrailingSpace, {
   desc = "delete trailing whitespace",
 })
+
+local function CaptureOutput(use_scractch, cmd_)
+  local more_opt = vim.go.more
+  vim.cmd("redir => captured")
+  vim.cmd("silent " .. cmd_)
+  vim.cmd("redir END")
+  if use_scractch then
+    vim.cmd("Sscratch")
+  end
+  vim.cmd("normal G")
+  vim.cmd("put=captured")
+  vim.o.more = more_opt
+end
+
+vim.api.nvim_create_user_command("CaptureOutput", function(args)
+  CaptureOutput(false, args.args)
+end, {
+  desc = "capture output",
+  nargs = "+",
+})
+
+vim.api.nvim_create_user_command("CaptureToSractch", function(args)
+  CaptureOutput(true, args.args)
+end, {
+  desc = "capture output to scratch buffer",
+  nargs = "+",
+})
+
+vim.keymap.set("n", "<Leader>oo", ":CaptureToSractch<space>")
+vim.keymap.set("n", "<Leader>oc", ":CaptureOutput<space>")
