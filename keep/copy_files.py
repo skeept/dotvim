2382abcdc4150c12f5196d0fb2e2da241646134e
Helder Inacio <Helder.Inacio@sabre.com>
2024-04-12 16:10:27 -0500

    copy_files.py: add ability to change windows conf

--- keep/copy_files.py
+++ keep/copy_files.py
@@ -1,13 +1,16 @@
 """Copy files to/from config folder."""
 
 import argparse
+import dataclasses
 import enum
-import os
 import filecmp
+import os
 import shutil
 import subprocess
-import dataclasses
 from pathlib import Path
+from typing import Optional
+
+WINDOWS_PATH = Path("/mnt/c/Users/SG0216489/AppData/Local")
 
 
 @dataclasses.dataclass
@@ -16,29 +19,29 @@ class FolderConfig:
     local: Path
 
     @classmethod
-    def nvim(cls):
+    def nvim(cls, windows: Optional[bool] = None):
         """Get nvim config."""
-        nvim_conf_dir = (
-            Path(os.path.expandvars(r"%LOCALAPPDATA%\nvim"))
+        conf_dir = (
+            Path(os.path.expandvars(r"%LOCALAPPDATA%"))
             if os.name == "nt"
-            else Path.home() / ".config/nvim"
+            else (WINDOWS_PATH if windows else Path.home() / ".config")
         )
-        return cls(actual=nvim_conf_dir / "lua", local=Path("nvim/lua"))
+        return cls(actual=conf_dir / "nvim/lua", local=Path("nvim/lua"))
 
     def flip(self):
         return FolderConfig(actual=self.local, local=self.actual)
 
 
-def get_all_elems(name: Path):
+def get_all_elements(name: Path):
     """Get all elements recursively."""
     for elem in name.iterdir():
         yield elem
         if elem.is_dir():
-            yield from get_all_elems(elem)
+            yield from get_all_elements(elem)
 
 
 class CmpStatus(enum.Enum):
-    """Status of file comparision."""
+    """Status of file comparison."""
 
     same = enum.auto()
     missing = enum.auto()
@@ -54,7 +57,7 @@ def copy_files(
 ) -> None:
     """Assume folders and copy nested files."""
     show_diff = not dry_run or show_diff
-    for elem in get_all_elems(origin):
+    for elem in get_all_elements(origin):
         status = CmpStatus.same
         backup = destination / elem.relative_to(origin)
         if not backup.exists():
@@ -62,10 +65,10 @@ def copy_files(
         elif not elem.is_dir() and not filecmp.cmp(elem, backup, shallow=False):
             status = CmpStatus.diff
         if display_all or status != CmpStatus.same:
-            print(f"{status.name:<7} {str(elem):<50} => {backup}")
+            print(f"{status.name:<7} {elem!s:<50} => {backup}")
             if show_diff and status == CmpStatus.diff and shutil.which("delta"):
                 delta_options = ["--side-by-side"]
-                subprocess.run(["delta", *delta_options, elem, backup])
+                subprocess.run(["delta", *delta_options, elem, backup], check=False)
 
         if not dry_run:
             if elem.is_dir():
@@ -75,18 +78,21 @@ def copy_files(
 
 
 def main() -> None:
-    """Setup args and copy."""
+    """Set args and copy."""
     parser = argparse.ArgumentParser("copy files from folder.")
     parser.add_argument(
-        "-o", "--other_direction", help="reverse copy destination", action="store_true"
+        "-o", "--other_direction", help="reverse copy destination", action="store_true",
     )
     parser.add_argument("-c", "--copy", action="store_true", help="copy the files")
     parser.add_argument(
-        "-d", "--hide_diff", action="store_true", help="hide diff output"
+        "-d", "--hide_diff", action="store_true", help="hide diff output",
+    )
+    parser.add_argument(
+        "-w", "--windows", action="store_true", help="use windows path", default=None,
     )
     args = parser.parse_args()
 
-    nvim = FolderConfig.nvim()
+    nvim = FolderConfig.nvim(args.windows)
     origin, dest = (
         dataclasses.astuple(nvim)
         if args.other_direction
