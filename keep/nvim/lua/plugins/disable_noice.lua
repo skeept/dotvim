7c818f0c13e0a4f0e8fc3a609521b410f26a920b
Helder Inacio <Helder.Inacio@Sabre.com>
2024-04-18 17:00:21 -0500

    revert back

--- keep/nvim/lua/plugins/disable_noice.lua
+++ keep/nvim/lua/plugins/disable_noice.lua
@@ -6,100 +6,102 @@
 -- * override the configuration of LazyVim plugins
 return {
 
-	{
-		"folke/noice.nvim",
-		enabled = false,
-		-- opts = {
-		--   cmdline = { enabled = false },
-		-- },
-	},
-	{
-		"folke/flash.nvim",
-		enabled = false,
-	},
-	-- Use <tab> for completion and snippets (supertab)
-	-- first: disable default <tab> and <s-tab> behavior in LuaSnip
-	{
-		"L3MON4D3/LuaSnip",
-		keys = function()
-			return {}
-		end,
-	},
-	-- then: setup supertab in cmp
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-emoji",
-		},
-		---@param opts cmp.ConfigSchema
-		opts = function(_, opts)
-			local has_words_before = function()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
+  {
+    "folke/noice.nvim",
+    enabled = false,
+    -- opts = {
+    --   cmdline = { enabled = false },
+    -- },
+  },
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  -- then: setup supertab in cmp
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0
+            and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
-			local luasnip = require("luasnip")
-			local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
 
-			opts.mapping = vim.tbl_extend("force", opts.mapping, {
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-					-- this way you will only jump inside the snippet region
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			})
-		end,
-	},
-	{
-		"cappyzawa/trim.nvim",
-		opts = {},
-	},
-	{
-		"https://git.sr.ht/~swaits/scratch.nvim",
-		lazy = true,
-		keys = {
-			{ "<leader>bs", "<cmd>Scratch<cr>", desc = "Scratch Buffer", mode = "n" },
-			{ "<leader>bS", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
-		},
-		cmd = {
-			"Scratch",
-			"ScratchSplit",
-		},
-		opts = {},
-	},
-	{
-		"ibhagwan/fzf-lua",
-		-- optional for icon support
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			-- calling `setup` is optional for customization
-			-- require("fzf-lua").setup({ "fzf-native" })
-			require("fzf-lua").setup({ "max-perf" })
-		end,
-	},
-	{
-		"akinsho/bufferline.nvim",
-		opts = { options = {
-			mode = "tabs",
-		} },
-	},
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+  {
+    "cappyzawa/trim.nvim",
+    opts = {},
+  },
+  {
+    "https://git.sr.ht/~swaits/scratch.nvim",
+    lazy = true,
+    keys = {
+      { "<leader>bs", "<cmd>Scratch<cr>",      desc = "Scratch Buffer",         mode = "n" },
+      { "<leader>bS", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
+    },
+    cmd = {
+      "Scratch",
+      "ScratchSplit",
+    },
+    opts = {},
+  },
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- calling `setup` is optional for customization
+      -- require("fzf-lua").setup({ "fzf-native" })
+      require("fzf-lua").setup({ "max-perf" })
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        mode = "tabs",
+      }
+    },
+  },
 }
