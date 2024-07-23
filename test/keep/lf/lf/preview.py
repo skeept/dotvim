cc2de28b198b26691b8f8c03962d8c31765e6867
Helder Inacio <Helder.Inacio@Sabre.com>
2024-07-23 10:44:40 -0500

    lf: remove . from extension
    also enable logging for debugging

--- test/keep/lf/lf/preview.py
+++ test/keep/lf/lf/preview.py
@@ -5,14 +5,26 @@ import shlex
 import subprocess
 import sys
 
+DEBUG = False
+
 file, *others = sys.argv[1:]
-_, suffix = os.path.splitext(file)
+_, suffix = os.path.splitext(file.lower())
+if suffix.startswith("."):
+    suffix = suffix[1:]
+
+
+if DEBUG:
+    import logging
+
+    logging.basicConfig(filename="log.txt", level=logging.WARNING)
+    logging.debug("suffix = %s", suffix)
 
 if suffix == "tar":
     subprocess.run(["tar", "tf", file], check=False)
-elif suffix in {"7z", "exe", "dll"}:
+elif suffix in {"7z", "exe", "dll", "zip"}:
     subprocess.run(["7z", "l", file], check=False)
 else:
     subprocess.run(
-        [*shlex.split("bat --style=plain --color=always"), file], check=False,
+        [*shlex.split("bat --style=plain --color=always"), file],
+        check=False,
     )
