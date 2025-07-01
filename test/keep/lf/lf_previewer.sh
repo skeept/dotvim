0234e059882f31000d2753f5b858d09a7dd4a4b1
Rodrigues, Jorge <jr@W15TDC54.com>
2025-07-01 18:29:17 -0500

    Update dotfiles/.visidatarc Update test/keep/lf/lf_previewer.sh

--- test/keep/lf/lf_previewer.sh
+++ test/keep/lf/lf_previewer.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 case "$1" in
-    *.tar*) tar tf "$1";;
-    *.zip) unzip -l "$1";;
-    *.whl) unzip -l "$1";;
-    *.rar) unrar l "$1";;
-    *.7z) 7z l "$1";;
-    *.pdf) pdftotext "$1" -;;
-    *) bat --style=plain --color=always  "$1";;
-    *.*) bat --style=plain --color=always  "$1";;
+*.tar*) tar tf "$1" ;;
+*.zip) unzip -l "$1" ;;
+*.whl) unzip -l "$1" ;;
+*.rar) unrar l "$1" ;;
+*.7z) 7z l "$1" ;;
+*.pdf) pdftotext "$1" - ;;
+*) bat --style=plain --color=always "$1" ;;
+*.*) bat --style=plain --color=always "$1" ;;
 esac
