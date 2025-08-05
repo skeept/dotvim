5d97a99d10ce057b38f945c01d714abe8e106cf2
Rodrigues, Jorge <jr@W15TDC54.com>
2025-08-05 18:41:29 -0500

    why so many changes

--- keep/nvim/lua/config/keymaps.lua
+++ keep/nvim/lua/config/keymaps.lua
@@ -15,70 +15,69 @@ vim.keymap.set({ "n", "v" }, "<F4>", ":x<CR>", { noremap = true })
 vim.keymap.set("i", "<F4>", "<C-O>:x<CR>", { noremap = true })
 
 vim.api.nvim_create_user_command("DeltTrailingSpace", function()
-  vim.cmd([[%s/\s\+$//e]])
+	vim.cmd([[%s/\s\+$//e]])
 end, {
-  desc = "delete trailing whitespace",
+	desc = "delete trailing whitespace",
 })
 
 local function CaptureOutput(use_scractch, cmd_)
-  local more_opt = vim.go.more
-  vim.cmd("redir => captured")
-  vim.cmd("silent " .. cmd_)
-  vim.cmd("redir END")
-  if use_scractch then
-    vim.cmd("Sscratch")
-  end
-  vim.cmd("normal G")
-  vim.cmd("put=captured")
-  vim.o.more = more_opt
+	local more_opt = vim.go.more
+	vim.cmd("redir => captured")
+	vim.cmd("silent " .. cmd_)
+	vim.cmd("redir END")
+	if use_scractch then
+		vim.cmd("Sscratch")
+	end
+	vim.cmd("normal G")
+	vim.cmd("put=captured")
+	vim.o.more = more_opt
 end
 
 vim.api.nvim_create_user_command("CaptureOutput", function(args)
-  CaptureOutput(false, args.args)
+	CaptureOutput(false, args.args)
 end, {
-  desc = "capture output",
-  nargs = "+",
+	desc = "capture output",
+	nargs = "+",
 })
 
 vim.api.nvim_create_user_command("CaptureToScratch", function(args)
-  CaptureOutput(true, args.args)
+	CaptureOutput(true, args.args)
 end, {
-  desc = "capture output to scratch buffer",
-  nargs = "+",
+	desc = "capture output to scratch buffer",
+	nargs = "+",
 })
 
 vim.keymap.set("n", "<Leader>oo", ":CaptureToScratch<space>")
 vim.keymap.set("n", "<Leader>oc", ":CaptureOutput<space>")
 
 vim.api.nvim_create_user_command("Pcp", function()
-  vim.cmd("echo expand('%:p')")
-  vim.fn.setreg("+", vim.fn.expand("%:p"))
+	vim.cmd("echo expand('%:p')")
+	vim.fn.setreg("+", vim.fn.expand("%:p"))
 end, {
-  desc = "Print current path and copy to clipboard",
+	desc = "Print current path and copy to clipboard",
 })
 
 local wk = require("which-key")
 wk.add({
-  { ",", group = "comma mappings" },
-  -- { ",e", "<cmd>Fzf files<cr>", desc = "files" },
-  {
-    ",e",
-    function()
-      require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
-    end,
-    desc = "files in pwd",
-  },
-  { ",b", "<cmd>FzfLua buffers<cr>", desc = "buffers" },
-  { ",f", group = "fzf" },
-  { ",ff", "<cmd>FzfLua<cr>", desc = "Fzf" },
-  { ",fg", "<cmd>FzfLua grep_project<cr>", desc = "grep project" },
-  { ",fr", "<cmd>FzfLua oldfiles<cr>", desc = "recent files" },
-  { ",fc", "<cmd>FzfLua commands<cr>", desc = "commands" },
-  { ",fh", "<cmd>FzfLua command_history<cr>", desc = "command history" },
-  { ",v", group = "search with vimgrep" },
-  { ",vn", ":vimgrep // *<Left><Left><Left>", desc = "vimgrep this folder (start typing)" },
-  { ",vr", ":vimgrep // **/*<Left><Left><Left><Left><Left><Left>", desc = "vimgrep nested" },
-  { ",s", group = "settings, tabs and more" },
-  { ",sw", "<cmd>setlocal wrap! | set wrap?<cr>", desc = "toogle wrap" },
-  { ",st", "<cmd>tab split<cr>", desc = "tab split" },
+	{ ",", group = "comma mappings" },
+	{
+		",e",
+		function()
+			require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
+		end,
+		desc = "files in pwd",
+	},
+	{ ",b", "<cmd>FzfLua buffers<cr>", desc = "buffers" },
+	{ ",f", group = "fzf" },
+	{ ",ff", "<cmd>FzfLua<cr>", desc = "Fzf" },
+	{ ",fg", "<cmd>FzfLua grep_project<cr>", desc = "grep project" },
+	{ ",fr", "<cmd>FzfLua oldfiles<cr>", desc = "recent files" },
+	{ ",fc", "<cmd>FzfLua commands<cr>", desc = "commands" },
+	{ ",fh", "<cmd>FzfLua command_history<cr>", desc = "command history" },
+	{ ",v", group = "search with vimgrep" },
+	{ ",vn", ":vimgrep // *<Left><Left><Left>", desc = "vimgrep this folder (start typing)" },
+	{ ",vr", ":vimgrep // **/*<Left><Left><Left><Left><Left><Left>", desc = "vimgrep nested" },
+	{ ",s", group = "settings, tabs and more" },
+	{ ",sw", "<cmd>setlocal wrap! | set wrap?<cr>", desc = "toogle wrap" },
+	{ ",st", "<cmd>tab split<cr>", desc = "tab split" },
 })
