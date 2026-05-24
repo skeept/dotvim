f224c773db208cbcd8da85452b1a6e611c0cd1a2
Rodrigues, Jorge <jr@W15TDC54.com>
2026-05-24 18:10:15 -0500

    lazy complete tv

--- test/keep/generate_lazy_completions.sh
+++ test/keep/generate_lazy_completions.sh
@@ -138,6 +138,7 @@ add_lazy_completion just _just "just --completions bash"
 add_lazy_completion gh __start_gh "gh completion -s bash"
 add_lazy_completion ruff _ruff "ruff generate-shell-completion ${CURSHELL}"
 add_lazy_completion chezmoi __start_chezmoi "chezmoi completion ${CURSHELL}"
+add_lazy_completion tv _tv " tv completions ${CURSHELL}"
 
 add_lazy_source yazi _yazi $HOME/bin/links/completions/yazi.bash
 add_lazy_source zoxide _zoxide ${HOME}/bin/links/completions/zoxide.bash
