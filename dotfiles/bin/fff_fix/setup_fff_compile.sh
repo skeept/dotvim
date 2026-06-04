1d8c409d2f31e55b555033bdb53eac1414cf68a3
Rodrigues, Jorge <jr@W15TDC54.com>
2026-06-04 16:43:29 -0500

    Add bin/fff_fix/zig Add bin/fff_fix/setup_fff_compile.sh

---
+++ dotfiles/bin/fff_fix/setup_fff_compile.sh
@@ -0,0 +1,19 @@
+# source this file instead of executing it
+#
+# install rust
+# sudo yum install
+#
+# check the right command
+
+# use zig wrapper
+export PATH=~/bin/fff_fix:${PATH}
+
+for dir in "/usr/lib64/llvm17/lib/clang/17/include" \
+  "/opt/rh/gcc-toolset-13/root/usr/lib/gcc/x86_64-redhat-linux/13/include" \
+  "/opt/rh/gcc-toolset-11/root/usr/lib/gcc/x86_64-redhat-linux/11/include" \
+  "/usr/lib/gcc/x86_64-redhat-linux/8/include"; do
+  if [ -d "$dir" ]; then
+    export BINDGEN_EXTRA_CLANG_ARGS="-isystem $dir"
+    break
+  fi
+done
