2604f170e2b435009ccf3f22ca61694c8823daaf
Rodrigues, Jorge <jr@W15TDC54.com>
2026-06-30 15:38:38 -0500

    update local.lua

--- keep/nvim/lua/plugins/local.lua
+++ keep/nvim/lua/plugins/local.lua
@@ -1,40 +1,28 @@
 return {
   {
     dir = vim.fn.stdpath("config") .. "/lua/wk",
-    name = "file_picker",
+    name = "wk_custom_tools",
     lazy = true,
-    cmd = { "FilePicker" },
+    -- Tell lazy.nvim all the commands this toolkit provides
+    cmd = { "FilePicker", "CaptureOutput", "CaptureToScratch", "DiffOtherFile" },
+    -- Tell lazy.nvim all the keys that should trigger this toolkit
     keys = {
+      -- File Picker
       { "<leader>fp", "<cmd>FilePicker<cr>", desc = "Open FilePicker" },
-    },
-    config = function()
-      require("wk.file_picker").setup()
-    end,
-  },
-  {
-    dir = vim.fn.stdpath("config") .. "/lua/wk",
-    name = "capture",
-    lazy = true,
-    cmd = { "CaptureOutput", "CaptureToScratch" },
-    keys = {
+
+      -- Capture Output
       { "<Leader>oo", ":CaptureToScratch<space>", desc = "Capture output to scratch" },
       { "<Leader>oc", ":CaptureOutput<space>", desc = "Capture output" },
       { ",cs", ":CaptureToScratch<space>", desc = "Capture output to scratch" },
       { ",co", ":CaptureOutput<space>", desc = "Capture output" },
-    },
-    config = function()
-      require("wk.capture").setup()
-    end,
-  },
-  {
-    dir = vim.fn.stdpath("config") .. "/lua/wk",
-    name = "diff_switcher",
-    lazy = true,
-    cmd = { "DiffOtherFile" },
-    keys = {
+
+      -- Diff Switcher (CRITICAL: Note the capital 'O' in DiffOtherFile)
       { "<leader>df", "<cmd>DiffOtherFile<cr>", desc = "Diff another file in BL/TR pairs" },
     },
     config = function()
+      -- Load all of your custom module setups together cleanly
+      require("wk.file_picker").setup()
+      require("wk.capture").setup()
       require("wk.diff_switcher").setup()
     end,
   },
