fdbfaebc9e791fe78daed0d0f546056ee8bd9585
Rodrigues, Jorge <jr@W15TDC54.com>
2026-06-29 20:42:30 -0500

    nvim: update nagivation keymaps

--- keep/nvim/lua/plugins/navigation.lua
+++ keep/nvim/lua/plugins/navigation.lua
@@ -5,6 +5,44 @@ return {
     config = function()
       require("fzf-lua").setup({ "max-perf" })
     end,
+    -- Add the keys block right here inside the fzf-lua spec
+    keys = {
+      {
+        "<leader>ff",
+        function()
+          LazyVim.pick("files", { root = false })()
+        end,
+        desc = "Find Files (cwd)",
+      },
+      {
+        "<leader>fF",
+        function()
+          LazyVim.pick("files")()
+        end,
+        desc = "Find Files (Root Dir)",
+      },
+      {
+        "<leader>sg",
+        function()
+          LazyVim.pick("live_grep", { root = false })()
+        end,
+        desc = "Grep (cwd)",
+      },
+      {
+        "<leader>sG",
+        function()
+          LazyVim.pick("live_grep")()
+        end,
+        desc = "Grep (Root Dir)",
+      },
+      {
+        "<leader>/",
+        function()
+          LazyVim.pick("live_grep", { root = false })()
+        end,
+        desc = "Grep (cwd)",
+      },
+    },
   },
   {
     "dmtrKovalenko/fff.nvim",
